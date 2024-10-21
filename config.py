8e18e1f6246a32067fece58d5b74bda6ebac8cc3
Matt Gaidica, Ph.D. <matt@gaidi.ca>
2024-10-20 19:00:03 -0500

    prettier

--- config.py
+++ config.py
@@ -1,23 +1,20 @@
 # config.py
 
-# examples:
-# from config import DATA_DIRECTORY
-# from S3Manager import needFile
+###############################################################
+# USER CONFIGURATION SECTION                                  #
+# Only edit the following values:                             #
+###############################################################
 
 SECRET_URL = "9612xJxgwx8q8at2NDIhqv4y"
 SECRET_DOMAIN = "https://hub-link.onrender.com/dashboard"
 
-# Base path for removable drive
-# BASE_PATH = '/media/gaidica/NULL/NullLink'
+###############################################################
 
 # Data location (removable drive)
 DATA_DIRECTORY = 'data' #f'{BASE_PATH}/data'
 
 # Database file location
 DATABASE_FILE = 'instance/hublink.db' #f'{BASE_PATH}/instance/hublink.db'
-
-# S3 bucket name
-BUCKET_NAME = 'neurotechhub-000'
 
 # Standardized datetime format
 DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S'
@@ -32,21 +29,3 @@ DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S'
 # 'never'   - No datetime component, returns an empty string
 # Any other value will raise a ValueError.
 VALID_DT_RULES = ['seconds', 'hours', 'days', 'weeks', 'months', 'years', 'never']
-DT_RULE = 'days'
-
-# File size rule
-MAX_FILE_SIZE = 5000000 # bytes
-
-# Use/upload to cloud
-USE_CLOUD = False
-
-# Scan deletion rules
-DELETE_SCANS = True
-DELETE_SCANS_DAYS_OLD = -1 # set to -1 to skip
-DELETE_SCANS_PERCENT_REMAINING = -1 # set to -1 to skip
-
-# Device filter by name
-DEVICE_NAME_INCLUDES = "ESP32"
-
-# Override MAC with ID file
-ID_FILE_STARTS_WITH = "id_"
